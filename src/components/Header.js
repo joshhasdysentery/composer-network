b301114593793098d60dc7f944e7d36ab270fd0c
joshua <wujoshua56@gmail.com>
2020-11-23 23:03:25 -0500

    name change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,31 @@
-import { React, Component } from 'react';
+import { React } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, IconButton } from '@material-ui/core';
+import { AppBar, IconButton, Toolbar } from '@material-ui/core';
 import SortIcon from '@material-ui/icons/Sort';
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  appbar: {
+    background: 'none',
+  },
+  appbarTitle: {
+    flexGrow: '1',
+  },
+  icon: {
+    color: '#fff',
+    fontSize: '2rem',
+  },
+}));
 
 export default function Header() {
   const classes = useStyles();
   return (
     <div>
-      <AppBar>
-        <h1>Dmitri Shostakovich</h1>
-        <IconButton>
-          <SortIcon />
-        </IconButton>
+      <AppBar className={classes.appbar}>
+        <Toolbar>
+          <h1 className={classes.appbarTitle}>Composer Network</h1>
+          <IconButton>
+            <SortIcon />
+          </IconButton>
+        </Toolbar>
       </AppBar>
     </div>
   );
