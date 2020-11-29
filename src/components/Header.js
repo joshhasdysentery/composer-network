a74515e8e3fe7af6231c009cb329d9d7f87fd76b
joshua <wujoshua56@gmail.com>
2020-11-29 00:42:17 -0500

    add title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
-import { React } from 'react';
+import { React, useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, IconButton, Toolbar } from '@material-ui/core';
+import { AppBar, IconButton, Toolbar, Collapse } from '@material-ui/core';
 import SortIcon from '@material-ui/icons/Sort';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    fontFamily: 'Merriweather',
+  },
   appbar: {
     background: 'none',
-    fontFamily: 'Merriweather',
   },
   appbarWrapper: {
     width: '80%',
@@ -18,12 +25,30 @@ const useStyles = makeStyles((theme) => ({
     color: '#fff',
     fontSize: '2rem',
   },
+  colorText: {
+    color: '#616A6B',
+  },
+  container: {
+    textAlign: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: '4.5rem',
+  },
+  goDown: {
+    color: '#fff',
+    fontSize: '15rem',
+  },
 }));
 
 export default function Header() {
   const classes = useStyles();
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  }, []);
   return (
-    <div>
+    <div className={classes.root}>
       <AppBar className={classes.appbar}>
         <Toolbar className={classes.appbarWrapper}>
           <h1 className={classes.appbarTitle}>
@@ -34,6 +59,22 @@ export default function Header() {
           </IconButton>
         </Toolbar>
       </AppBar>
+
+      <Collapse
+        in={checked}
+        {...(checked ? { timeout: 1000 } : {})}
+        collapsedHeight={50}
+      >
+        <div className={classes.container}>
+          <h1 className={classes.title}>
+            <br />
+            Composer <span className={classes.colorText}>Network</span>
+          </h1>
+          <IconButton>
+            <ExpandMoreIcon classes={classes.goDown} />
+          </IconButton>
+        </div>
+      </Collapse>
     </div>
   );
 }
