3b0d16dca257397101c289895aad881a93980838
joshua <wujoshua56@gmail.com>
2020-12-09 22:26:45 -0500

    help

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
 import './Footer.css';
-import { Domain } from 'material-ui-icons';
 
 function Footer() {
   return (
